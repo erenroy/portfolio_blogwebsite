0878cfbd811376b243d04a517c4b47d3e8f14de1
erenroy <roypapan75@gmail.com>
2023-09-19 20:34:37 +0530

    add blog code
    Adding the blog code

---
+++ icoder/blog/urls.py
@@ -0,0 +1,10 @@
+# This is blog urls
+
+from django.contrib import admin
+from django.urls import path , include
+from . import views 
+
+urlpatterns = [
+    path('', views.blogHome , name="blogHome"),
+    path('<str:slug>', views.blogPost , name="blogPost"),
+]
---
+++ icoder/home/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render , HttpResponse
+from home.models import Contact
+from django.contrib import messages
+from blog.models import Post
+
+# Create your views here.
+def home(request):
+    return render(request, 'home/home.html')
+    
+def about(request):
+    return render(request, 'home/about.html')
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+#        print(name,phone,content,email)
+
+        if len(name)<2 or len(email)<3 or len(phone)<10 or len(content)<4:
+            messages.error(request, "Please fill the form correctly ! ")
+        else:
+            contact = Contact(name=name, email=email , phone=phone , content=content)
+            contact.save()
+            messages.success(request, "your message has been sent ")
+        
+    return render(request, 'home/contact.html')
+
+
+def search(request):
+    query = request.GET['query']
+    allPosts = Post.objects.filter(title__icontains=query)
+    params = {'allPosts' : allPosts}
+    return render(request,'home/search.html' , params)
+#   return HttpResponse("This is search")
